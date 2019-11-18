f8ed6aca70e55088c0d5a146b9f3599397f82281
Martin Civan <martin.civan5@gmail.com>
2019-11-18 21:52:41 +0100

    test fix

--- tests/test_metrix.py
+++ tests/test_metrix.py
@@ -2,21 +2,22 @@ from unittest import TestCase
 
 from server.metrix.acceleration import Acceleration
 from server.metrix.velocity import Velocity
+from server.models.movement import Movement
 
-movements = [{"session_id": "test",
-              "user_id": "test",
-              "timestamp": i,
-              "controller_id": "test",
-              "x": 2 ** i,
-              "y": 2 ** i,
-              "z": 2 ** i,
-              "yaw": "test",
-              "pitch": "test",
-              "roll": "test",
-              "r_x": "test",
-              "r_y": "test",
-              "r_z": "test",
-              } for i in range(11)]
+movements = [Movement.from_dict({"session_id": "test",
+                                 "user_id": "test",
+                                 "timestamp": i,
+                                 "controller_id": "test",
+                                 "x": 2 ** i,
+                                 "y": 2 ** i,
+                                 "z": 2 ** i,
+                                 "yaw": "test",
+                                 "pitch": "test",
+                                 "roll": "test",
+                                 "r_x": "test",
+                                 "r_y": "test",
+                                 "r_z": "test",
+                                 }) for i in range(11)]
 
 
 class TestMetrix(TestCase):
