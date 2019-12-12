08de63ff80e3316f093a21c8d2cff53d030aa961
Martin Civáň <martin.civan5@gmail.com>
2019-12-12 19:28:35 +0100

    metrix scaling

--- server/lookup/faiss.py
+++ server/lookup/faiss.py
@@ -1,6 +1,7 @@
 import faiss
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 from server.db import create_db
 
@@ -11,18 +12,25 @@ class FaissIndexFlatL2():
         super().__init__()
         self.db = create_db()
         df = pd.DataFrame(list(self.db.get_all_metrix()))
-        df = df.drop("_id", axis="columns")
         self.user_ids = df["user_id"]
         df = df.drop("user_id", axis="columns")
-        df = df.drop("session_id", axis="columns")
+        try:
+            df = df.drop("_id", axis="columns")
+        except KeyError:
+            pass
+        try:
+            df = df.drop("session_id", axis="columns")
+        except KeyError:
+            pass
         self.index = faiss.IndexFlatL2(len(df.columns))
-        self.add(df.to_numpy("float32"))
+        self.scaler = StandardScaler()
+        self.add(self.scaler.fit_transform(df.to_numpy("float32")))
 
     def add(self, vectors):
         self.index.add(np.ascontiguousarray(vectors))
 
     def search(self, data, k):
-        distances, indices = self.index.search(data, k)
+        distances, indices = self.index.search(np.ascontiguousarray(self.scaler.transform(data)), k)
         results = []
         for i, d in zip(indices[0], distances[0]):
             if i > -1:
