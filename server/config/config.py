9187b7cda9d102a010e64079b1e23872d5a3349c
Martin Civan <martin.civan5@gmail.com>
2019-12-22 21:05:25 +0100

    config fix

--- server/config/config.py
+++ server/config/config.py
@@ -4,5 +4,5 @@ DB_HOST = "mongodb://" + os.environ["db_host"] + ":" + os.environ["db_port"] + "
 DB_NAME = "behaworks_logger_v7"
 DB_PORT = ""
 URL_PREFIX = "/api"
-METRIX_CHUNKS = 3
-NEIGHBOURS = 3
+METRIX_CHUNKS = os.environ["metrix_chunks"]
+NEIGHBOURS = os.environ["neighbours"]
