b662c5d8f635a5392b8883e370247748c1d07a6f
MatusPilnan <m.pilnan@gmail.com>
2019-12-23 12:55:08 +0100

    update actions

--- server/config/config.py
+++ server/config/config.py
@@ -4,5 +4,5 @@ DB_HOST = "mongodb://" + os.environ["db_host"] + ":" + os.environ["db_port"] + "
 DB_NAME = "behaworks_logger_v7"
 DB_PORT = ""
 URL_PREFIX = "/api"
-METRIX_CHUNKS = os.environ["metrix_chunks"]
-NEIGHBOURS = os.environ["neighbours"]
+METRIX_CHUNKS = int(os.environ["metrix_chunks"])
+NEIGHBOURS = int(os.environ["neighbours"])
