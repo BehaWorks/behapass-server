4d89895803a5c4dc0306849507de006d22839fa2
Andrej Schmidt <andrej.schmidt98@gmail.com>
2020-03-05 12:08:49 +0100

    changed metric

--- server/metrix/time_length.py
+++ server/metrix/time_length.py
@@ -5,5 +5,7 @@ from server.metrix.result import Result
 class TimeLength(Metric):
     def calculate(self, movements: list) -> Result:
         timestamps = self.extract_timestamps(movements)
-        time_length = timestamps[-1]
-        return Result(time_length)
+
+        timestamps_unified = [timestamps[-1] for _ in timestamps]
+
+        return Result(timestamps_unified)
