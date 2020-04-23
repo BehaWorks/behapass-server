e0f5a6efc2a9fd9159fd0290bae4f145044f5392
Matus Kovac <matus.kovac2@gmail.com>
2020-04-23 13:49:22 +0200

    delete buttons metrix

--- utils/compute_metrix.py
+++ utils/compute_metrix.py
@@ -1,11 +1,10 @@
 from server import create_db
 from server.metrix import create_metrix_vector
-from server.models.button import Button
 from server.views.api import split_movements
 
 db = create_db()
 
-new_db = db.mongo["behaworks_logger_v8"]
+new_db = db.mongo["behaworks_logger_v10"]
 # movement_collection = new_db["movement"]
 # button_collection = new_db["button"]
 metrix_collection = new_db["metrix"]
@@ -16,8 +15,6 @@ metrix_collection = new_db["metrix"]
 for session_id in db.get_session_ids():
     try:
         metrix_collection.insert_one(
-            create_metrix_vector(*split_movements(db.get_movements_by_session_id(session_id)),
-                                 [Button.from_dict(button) for button in
-                                  db.get_buttons_by_session_id(session_id)]).to_dict())
+            create_metrix_vector(*split_movements(db.get_movements_by_session_id(session_id))).to_dict())
     except (ValueError, IndexError):
         print("ZLE: " + session_id)
